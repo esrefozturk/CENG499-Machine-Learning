67a3700a15a6abc3402bf1021ff41438bf3051bc
Esref Ozturk <esrefozturk93@gmail.com>
2017-11-06 21:43:00 +0300

    input reading fixed

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -4,23 +4,32 @@ FILE = 'pima-indians-diabetes.csv'
 
 
 def get_data():
-    lines = [i.strip().split(',') for i in open(FILE).read().strip().split('\n')]
-    X = []
-    Y = []
-    for line in lines:
-        X.append(line[:-1])
-        Y.append(line[-1])
-    trainX = np.array(X[:668], dtype=np.float64)
-    testX = np.array(X[668:], dtype=np.float64)
-    trainY = np.array(Y[:668], dtype=np.float64).reshape(-1, 1)
-    testY = np.array(Y[668:], dtype=np.float64).reshape(-1, 1)
-    return trainX, testX, trainY, testY
+    data = np.loadtxt(FILE, delimiter=",")
+
+    train = data[:558]
+    validation = data[558:668]
+    test = data[668:]
+
+    trainX = scale(train[:, :-1])
+    trainY = train[:, -1].reshape(-1, 1)
+
+    validationX = scale(validation[:, :-1])
+    validationY = validation[:, -1].reshape(-1, 1)
+
+    testX = scale(test[:, :-1])
+    testY = test[:, -1].reshape(-1, 1)
+
+    np.random.seed(499)
+    w = np.random.random_sample(trainX.shape[1]).reshape(-1, 1)
+
+    return trainX, trainY, validationX, validationY, testX, testY, w
 
 
 def scale(x):
     mi = np.amin(x, axis=0)
     ma = np.amax(x, axis=0)
-    return (x - mi) / (ma - mi)
+    scaled = (x - mi) / (ma - mi)
+    return np.append(np.ones((scaled.shape[0], 1)), scaled, axis=1)
 
 
 def sigmoid(x, w):
@@ -45,15 +54,8 @@ def minimize_loss(x, w, y, num_iters=10000, eta=3e-4):
 
 
 def main():
-    trainX, testX, trainY, testY = get_data()
-    trainX = scale(trainX)
-    trainX = np.append(np.ones((trainX.shape[0], 1)), trainX, axis=1)
+    trainX, trainY, validationX, validationY, testX, testY, w = get_data()
 
-    testX = scale(testX)
-    testX = np.append(np.ones((testX.shape[0], 1)), testX, axis=1)
-
-    np.random.seed(499)
-    w = np.random.random_sample(trainX.shape[1]).reshape(-1, 1)
     w = minimize_loss(trainX, w, trainY)
 
     asdas = sigmoid(testX, w) >= .5
