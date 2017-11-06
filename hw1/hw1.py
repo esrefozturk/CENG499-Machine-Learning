dc2a97b5b0bdb9fb2eb4a65da1aee5348d2dad7a
Esref Ozturk <esrefozturk93@gmail.com>
2017-11-06 15:09:30 +0300

    x and w vectors are ready

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -8,7 +8,7 @@ def get_data():
     X = []
     Y = []
     for line in lines:
-        X.append([1] + lines[:-1])
+        X.append([1] + line[:-1])
         Y.append(line[-1])
     trainX = np.array(X[:668])
     testX = np.array(X[668:])
@@ -17,8 +17,15 @@ def get_data():
     return trainX, testX, trainY, testY
 
 
+def sigmoid(x, w):
+    t = np.dot(w.T, x)
+    return 1 / (1 - np.exp(-t))
+
+
 def main():
-    print get_data()
+    trainX, testX, trainY, testY = get_data()
+    np.random.seed(499)
+    w = np.concatenate([[0], np.random.random_sample(trainX.shape[1])])
 
 
 if __name__ == '__main__':
