925e8fb52681bfba43281e5b0f3323943d0bcc97
Esref Ozturk <esrefozturk93@gmail.com>
2017-11-06 16:37:33 +0300

    scaling

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -8,24 +8,45 @@ def get_data():
     X = []
     Y = []
     for line in lines:
-        X.append([1] + line[:-1])
+        X.append(line[:-1])
         Y.append(line[-1])
-    trainX = np.array(X[:668])
-    testX = np.array(X[668:])
-    trainY = np.array(Y[:668])
-    testY = np.array(Y[668:])
+    trainX = np.array(X[:668], dtype=np.float64)
+    testX = np.array(X[668:], dtype=np.float64)
+    trainY = np.array(Y[:668], dtype=np.float64).reshape(-1, 1)
+    testY = np.array(Y[668:], dtype=np.float64).reshape(-1, 1)
     return trainX, testX, trainY, testY
 
 
+def scale(x):
+    mi = np.amin(x, axis=0)
+    ma = np.amax(x, axis=0)
+    return (x - mi) / (ma - mi)
+
+
 def sigmoid(x, w):
-    t = np.dot(w.T, x)
+    t = np.dot(x, w)
     return 1 / (1 - np.exp(-t))
+
+
+def minimize_loss(x, w, y, num_iters=1000, eta=0.001):
+    for i in range(num_iters):
+        s = sigmoid(x, w).reshape(-1, 1)
+
+        dw = np.sum((y - s) * x) / float(x.shape[0])
+
+        w -= eta * dw
+    return w
 
 
 def main():
     trainX, testX, trainY, testY = get_data()
+    trainX = scale(trainX)
+    trainX[:, :-1] = np.ones((trainX.shape[0], 1))
+
     np.random.seed(499)
-    w = np.concatenate([[0], np.random.random_sample(trainX.shape[1])])
+    w = np.concatenate([[0], np.random.random_sample(trainX.shape[1] - 1)]).reshape(-1, 1)
+    minimize_loss(trainX, w, trainY)
+    print w
 
 
 if __name__ == '__main__':
