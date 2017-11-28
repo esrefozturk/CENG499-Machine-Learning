72dd41738a59efcd71a727895a30aa59e6d49f1a
Esref Ozturk <esrefozturk93@gmail.com>
2017-11-28 19:35:56 +0300

    all hoca defined tests are passed

--- hw2/driver.py
+++ hw2/driver.py
@@ -14,3 +14,8 @@ np.random.seed(499)
 
 # Test the affine_forward function
 test_affine_forward()
+test_affine_backward()
+test_relu_forward()
+test_relu_backward()
+test_L2_loss()
+test_ANN_predict()
