bf0251c87852dcc8334617d30ddffb3f5e504a30
Esref Ozturk <esrefozturk93@gmail.com>
2017-12-26 19:39:04 +0300

    pca started

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -21,23 +21,24 @@ def findClusterCenters(images, K):
         print centers
         SUMS = {c: 0 for c in centers}
         LENS = {c: 0 for c in centers}
+
         def assign1(x):
             SUMS[centers[(np.abs(np.array(centers) - x)).argmin()]] += x
             LENS[centers[(np.abs(np.array(centers) - x)).argmin()]] += 1
             return x
+
         f = np.vectorize(assign1)
         f(images)
         new_centers = []
         for i in SUMS:
             if LENS[i]:
-                new_centers.append(SUMS[i]/LENS[i])
+                new_centers.append(SUMS[i] / LENS[i])
             else:
                 new_centers.append(i)
         new_centers = sorted(new_centers)
         if centers == new_centers:
             break
         centers = new_centers
-
 
     return centers
 
@@ -50,18 +51,25 @@ def kmeansCompress(images, centers):
     return f(images)
 
 
-def main():
+def kmeans():
     images = np.load('FaceImages.npy')
 
-    #images = images[:10]
+    # images = images[:10]
 
     centers = findClusterCenters(images, 2)
-
 
     images = kmeansCompress(images, centers)
 
     for i in range(len(images)):
         show(images[i])
+
+
+def findPrincipalComponents(images, K):
+    pass
+
+
+def main():
+    images = np.load('FaceImages.npy')
 
 
 if __name__ == '__main__':
