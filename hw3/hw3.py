b06df3e2330bef3f13b0050f82c025601e44e684
Esref Ozturk <esrefozturk93@gmail.com>
2018-01-01 19:56:42 +0300

    hw3 last versions of methods

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,10 +1,52 @@
 import numpy as np
 
-from function import show
-
 
 def random_k_points(K):
     return np.sort(np.random.randint(0, 255, K))
+
+
+def fast_assign_points(centers, d):
+    assigned_centers = {i: [] for i in centers}
+    for i in d:
+        n = assigned_centers.keys()[0]
+        for j in assigned_centers:
+            if abs(j - i) < abs(n - i):
+                n = j
+        assigned_centers[n].append(i)
+    return assigned_centers
+
+
+def fast_get_new_centers(assigned_centers, d):
+    centers = []
+    for c in assigned_centers:
+        s = 0
+        l = 0
+        for i in assigned_centers[c]:
+            s += d[i] * i
+            l += d[i]
+        if l:
+            centers.append(s / l)
+        else:
+            centers.append(c)
+    return centers
+
+
+def fast_findClusterCenters(images, K):
+    images = images.reshape(-1).tolist()
+    centers = random_k_points(K).tolist()
+
+    d = {}
+    for i in images:
+        d[i] = d.get(i, 0) + 1
+
+    while 1:
+        assigned_centers = fast_assign_points(centers, d)
+        new_centers = sorted(fast_get_new_centers(assigned_centers, d))
+        if new_centers == centers:
+            break
+        centers = new_centers
+
+    return np.array(centers)
 
 
 def findClusterCenters(images, K):
@@ -12,7 +54,6 @@ def findClusterCenters(images, K):
     centers = random_k_points(K)
 
     while 1:
-        print centers
         new_centers = np.copy(centers)
         d = np.abs(centers.reshape(-1, 1) - images)
         d = np.argmin(d, axis=0)
@@ -36,7 +77,6 @@ def kmeansCompress(images, centers):
     s = np.zeros(images.shape[0])
 
     for i, c in enumerate(centers):
-        print s
         s += (d == i) * c
 
     return s.reshape(-1, 45045)
@@ -47,8 +87,7 @@ def kmeans(images, K):
 
     images = kmeansCompress(images, centers)
 
-    for i in range(len(images[:1])):
-        show(images[i])
+    return images
 
 
 def findPrincipalComponents(images, K):
@@ -58,20 +97,25 @@ def findPrincipalComponents(images, K):
 
 
 def pcaCompress(images, pca):
+    images = (images - np.mean(images, axis=0)) / np.std(images, axis=0)
     c = np.dot(images, pca)
     return np.dot(c, pca.T)
 
 
+def pca(images, K):
+    p = findPrincipalComponents(images, K)
+
+    images = pcaCompress(images, p)
+
+    return images
+
+
 def main():
     images = np.load('FaceImages.npy')
+    K = 8
+    kmeans(np.copy(images), K)
 
-    kmeans(images, 2)
-
-    # pca = findPrincipalComponents(images, 2)
-
-    # images = pcaCompress(images, pca)
-
-    # show(images[0])
+    pca(np.copy(images), K)
 
 
 if __name__ == '__main__':
